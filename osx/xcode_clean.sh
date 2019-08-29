978e78bd0652fd0cf844b97064c44a4919f9038d
wansong <betterofsong@gmail.com>
2019-08-29 10:41:12 +0800

    xcode_clean

---
+++ osx/xcode_clean.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+
+if pgrep -i xcode >/dev/null ; then
+  echo "Xcode is working, not doing it."
+  exit 0
+fi
+
+declare -a clean_list=( \
+  ~/Library/Developer/Xcode/DerivedData/* \
+  ~/Library/Developer/Xcode/Archives/* \
+  ~/Library/Developer/Xcode/iOS\ Device\ Logs/* \
+  )
+
+check_path() {
+  pref=~/Library/Developer/Xcode/
+  if ! echo "$1" | grep -E "^$pref" >/dev/null ; then
+    return 1
+  fi
+  return 0
+}
+
+for ff in "${clean_list[@]}" ; do
+  if [ -z "$ff" ] || [ ! -e "$ff" ] ; then
+    continue
+  fi
+  if ! check_path "$ff" ; then
+    echo "Path($ff) is not safe to rm -rf"
+    continue
+  fi
+  set -x
+  rm -rf "$ff"
+  set +x
+done
