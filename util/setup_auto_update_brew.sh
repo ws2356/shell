bcd6730e0ffcf916b5dca69d70d2b5307794af56
wansong <betterofsong@gmail.com>
2019-09-16 14:38:28 +0800

    setup_auto_update_brew

---
+++ util/setup_auto_update_brew.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+set -e
+
+label=${1:-work.mac.brew.update}
+plist_file=~/Library/LaunchAgents/"$label".plist
+log_file=/var/log/"${label}".log
+error_file=/var/log/"${label}".error.log
+user=$(id -nu)
+
+# plist 文件不能包含下面三个字符，否则需要转译\`$
+cat >"$plist_file" <<EOFPLIST
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>KeepAlive</key>
+	<dict>
+		<key>SuccessfulExit</key>
+		<false/>
+	</dict>
+	<key>Label</key>
+	<string>${label}</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>/usr/local/bin/brew</string>
+		<string>update</string>
+	</array>
+	<key>RunAtLoad</key>
+	<false/>
+	<key>StandardErrorPath</key>
+	<string>${error_file}</string>
+	<key>StandardOutPath</key>
+	<string>${log_file}</string>
+	<key>UserName</key>
+	<string>${user}</string>
+        <key>ExitTimeOut</key>
+        <integer>3599</integer>
+        <key>StartInterval</key>
+        <integer>3600</integer>
+</dict>
+</plist>
+EOFPLIST
+
+chmod 644 "$plist_file"
+
+touch "$error_file"
+touch "$log_file"
