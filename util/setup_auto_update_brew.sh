cfb63543a3b6a73dbb065bf62622d3fa21400e97
wansong <betterofsong@gmail.com>
2019-09-21 11:35:21 +0800

    install as LaunchDaemon

--- util/setup_auto_update_brew.sh
+++ util/setup_auto_update_brew.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 set -e
 
-label=${1:-work.mac.brew.update}
-plist_file=~/Library/LaunchAgents/"$label".plist
+if [ "$(id -u)" -eq 0 ] ; then
+  echo "Must not be called as root as required by homebrew."
+fi
+
+user=$(id -nu)
+label=${1:-${user}.homebrew.auto_update}
+plist_file=/Library/LaunchDaemons/"$label".plist
 log_file=/var/log/"${label}".log
 error_file=/var/log/"${label}".error.log
-user=$(id -nu)
+
+if [ -f "$plist_file" ] ; then
+  if ! command xmlstarlet >/dev/null 2>&1 ; then
+    brew install xmlstarlet
+  fi
+  existing_label=$(xmlstarlet sel -T -t \
+    -v '//dict/key[text()="Label"]/following-sibling::*[1]' "$plist_file" \
+    2>/dev/null)
+  if [ -n "$existing_label" ] && launchctl list "$existing_label" >/dev/null 2>&1 ; then
+    echo "Existing plist file is valid, you should unload it beforehand."
+    exit 1
+  fi
+fi
 
 # plist 文件不能包含下面三个字符，否则需要转译\`$
-cat >"$plist_file" <<EOFPLIST
+sudo tee "$plist_file" <<EOFPLIST
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -41,13 +58,21 @@ cat >"$plist_file" <<EOFPLIST
 </plist>
 EOFPLIST
 
+sudo chown "$(sudo id -nu):$(sudo id -ng)" "$plist_file"
 chmod 644 "$plist_file"
 
 sudo touch "$error_file"
-sudo chown "$(id -nu):$(id -ng)" $_
-sudo touch "$log_file"
-sudo chown "$(id -nu):$(id -ng)" $_
+sudo chown "$(id -nu):$(id -ng)" "$_"
+sudo chmod 644 "$_"
 
-if ! launchctl load "$plist_file" ; then
+sudo touch "$log_file"
+sudo chown "$(id -nu):$(id -ng)" "$_"
+sudo chmod 644 "$_"
+
+if ! sudo launchctl load "$plist_file" ; then
   echo "Failed to load plist_file: $plist_file"
+fi
+
+if ! grep -E '^[^#]*HOMEBREW_NO_AUTO_UPDATE' ~/.bash_profile >/dev/null 2>&1 ; then
+  echo 'export HOMEBREW_NO_AUTO_UPDATE=1' >> ~/.bash_profile
 fi
