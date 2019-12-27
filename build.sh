74d1a2c0b2d91a3f03c50b2dfb2c3f05bdebfb60
ws <wansong@kuaishou.com>
2019-12-27 22:30:05 +0800

    update simlog to interactive

--- build.sh
+++ build.sh
@@ -1,4 +1,27 @@
 #!/usr/bin/env bash
+install_dir=${1:-}
+
 [ -d build ] || mkdir build
 ./shellpack core fs ios mobile osx >build/install.sh
 chmod +x build/install.sh
+
+if [ -z "$install_dir" ] ; then
+  exit
+fi
+
+[ -d "$install_dir" ] || mkdir -p "$install_dir"
+old_contents=$(ls -Al "$install_dir")
+if [ -n "$old_contents" ] ; then
+  read -p \
+    "$install_dir has following contents: ${old_contents}"$'\n'" u sure to remove them(true|false)?"$'\n' \
+    -r confirm
+
+  if ! [ "$(echo "$confirm" | sed 's/[[:space:]]//g')" = 'true' ] ; then
+    exit
+  fi
+fi
+
+install_sh=$(pwd)/build/install.sh
+rm -rf "${install_dir}/*"
+cd "${install_dir}"
+"$install_sh"
