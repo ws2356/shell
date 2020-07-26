2406d784dee61aac67d48bda4db3cbab4e2fb6b7
wansong <betterofsong@gmail.com>
2020-07-26 14:29:46 +0800

    mdf

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 set -eu
+
+is_force=false
+while [ "$#" -gt 0 ] ; do
+  case "$1" in
+    -f)
+      is_force=true
+        shift
+        ;;
+    *)
+        shift
+        ;;
+  esac
+done
+
 default_install_dir=~/.config/shellpack/bin
 install_dir=${1:-$default_install_dir}
 
@@ -9,7 +23,7 @@ chmod +x build/install.sh
 
 [ -d "$install_dir" ] || mkdir -p "$install_dir"
 old_contents=$(ls -l "$install_dir")
-if [ -n "$old_contents" ] ; then
+if [ -n "$old_contents" ] && ! $is_force ; then
   read -p \
     "$install_dir has following contents: ${old_contents}"$'\n'" u sure to remove them(true|false)?"$'\n' \
     -r confirm
